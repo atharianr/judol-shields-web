5f4cc1ff5175ecf54a8b813b5d9bc87bd2135921
atharianr <atharianrahmadani@gmail.com>
2025-06-01 17:10:43 +0700

    feat: add handler on back button clicked

--- src/app/PageContent.jsx
+++ src/app/PageContent.jsx
@@ -10,10 +10,18 @@ export default function PageContent() {
             <div className="flex flex-col w-max justify-center">
                 <h1 className="text-6xl text-[#2F3396] font-bold ml-36">Ooops....</h1>
                 <p className="text-[#6B6FC2] text-xl mt-6 ml-36">Wah, konten di halaman ini terindikasi kurang cocok. Yuk, jelajahi halaman lainnya!</p>
-                <button className="w-fit bg-[#2F3396] text-white text-xl font-medium px-12 py-3 mt-8 ml-36 rounded-2xl cursor-pointer">Kembali</button>
+                <button
+                    onClick={() => window.history.back()}
+                    className="w-fit bg-[#2F3396] text-white text-xl font-medium px-12 py-3 mt-8 ml-36 rounded-2xl cursor-pointer hover:bg-[#2F3396]/80 hover:shadow-sm duration-200"
+                >
+                    Kembali
+                </button>
+                {/* <button className="w-fit text-[#6B6FC2] text-sm ml-36 mt-4 underline cursor-pointer">
+                    Laporkan jika halaman ini bukan Judol — <span className="font-bold">klik di sini</span>.
+                </button> */}
             </div>
             <div className="flex w-full items-center justify-center">
-                <img src="./assets/OnlineGamblingIllustration.svg" alt="Illustration" draggable="false" className="h-[500px] w-fit" />
+                <img src="./assets/OnlineGamblingIllustration.svg" alt="Online Gambling Illustration" draggable="false" className="h-[500px] w-fit" />
             </div>
         </div>
     )
